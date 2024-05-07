b6207208c33dc0128bcc42871880d1be613038df
Tomasz Smolarek <tomasz.smolarek@escolasoft.com>
2024-05-07 16:57:23 +0200

    Init package

---
+++ src/EscolaLmsRevenueCatIntegrationServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace EscolaLms\RevenueCatIntegration;
+
+use EscolaLms\Auth\EscolaLmsAuthServiceProvider;
+use EscolaLms\Settings\EscolaLmsSettingsServiceProvider;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * SWAGGER_VERSION
+ */
+class EscolaLmsRevenueCatIntegrationServiceProvider extends ServiceProvider
+{
+    const CONFIG_KEY = 'escolalms_revenuecat_integration';
+
+    public const REPOSITORIES = [];
+
+    public const SERVICES = [];
+
+    public $singletons = self::SERVICES + self::REPOSITORIES;
+
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/config.php', self::CONFIG_KEY);
+
+        $this->app->register(EscolaLmsSettingsServiceProvider::class);
+        $this->app->register(EscolaLmsAuthServiceProvider::class);
+    }
+
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/routes.php');
+
+        if ($this->app->runningInConsole()) {
+            $this->bootForConsole();
+        }
+    }
+
+    public function bootForConsole()
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+
+        $this->publishes([
+            __DIR__ . '/config.php' => config_path(self::CONFIG_KEY . '.php'),
+        ], self::CONFIG_KEY . '.config');
+    }
+}
